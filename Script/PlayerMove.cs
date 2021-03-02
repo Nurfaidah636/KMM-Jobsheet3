e06fda4551d1fd37090a49582e98b7d4613d2ea2
Nurfaidah636 <70933551+Nurfaidah636@users.noreply.github.com>
2021-03-03 05:57:40 +0700

    Add files via upload

---
+++ Script/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10;
+    private Rigidbody2D rigidbody2D;
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        float yMove = Input.GetAxis("Vertikal");
+        float xSpeed = xMove * speed;
+        float ySpeed = yMove * speed;
+        Vector2 newVelocity = new Vector2(xSpeed, ySpeed);
+        rigidbody2D.velocity = newVelocity;
+    }
+}
